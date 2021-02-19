f95302114c7132f45f28089d7af9da6afadba955
Rossokhach_KSU <o.rossokhach@yandex.ru>
2021-02-19 23:25:21 +0300

    Проект переделан под Maven

---
+++ sand/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+  public static void main(String[] args) {
+    hello("world");
+    hello("Oxana");
+    hello("user");
+    Square s = new Square(5);
+    System.out.println("Площаь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(4,6);
+    r.a = 4;
+    r.b = 6;
+    System.out.println("Площадь прямоугольника со сторнами " + r.a + " и " + r.b + " = " + r.area());
+  }
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
+
+
+}
